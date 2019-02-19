ace82326dd32db169ccfbeff55c5dc482a9540cd
Kamil Mirosz <kmirosz6@gmail.com>
2019-02-20 00:01:57 +0100

    add header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
-import Image from '../components/Image';
+
+import Logo from '../assets/images/logo.png';
 
 const Page = styled.div`
   width: 100%;
   height: 100vh;
   padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
 `;
 
+const Header = styled.header`
+  width: 100%;
+  padding: 20px 0;
+`;
+
+const Brand = styled.img`
+  max-width: 200px;
+`;
+
+const TextSection = styled.section`
+  padding: 10vh 0;
+`;
+
+const Title = styled.h1`
+  font-size: 25px;
+  margin-bottom: 10px;
+`;
+
+const Text = styled.h1`
+  font-size: 16px;
+`;
 
 const IndexPage = () => (
   <Layout>
     <Page>
-      <h1>Portfolio</h1>      
+      <Header>
+        <Link to="/">
+          <Brand src={Logo} alt="" />
+        </Link>
+      </Header>
+      <TextSection>
+        <Title>We are Developers</Title>
+        <Text>We made websites.</Text>
+      </TextSection>
     </Page>
   </Layout>
 );
